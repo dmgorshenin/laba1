d851c0a9032259b05ae336e4738afdd436e14b82
dmgorshenin <gorsh03@gmail.com>
2022-09-15 09:12:19 +0400

    commit

---
+++ laba1.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+from re import sub
+from decimal import Decimal
+import io
+from datetime import datetime
+import pandas as pd
+import os
+import cv2
+
+
