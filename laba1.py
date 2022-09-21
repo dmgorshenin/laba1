7bc169664432b436aea4f94ecb794b6bf09b52de
dmgorshenin <gorsh03@gmail.com>
2022-09-22 01:08:20 +0400

    функция cat переделана как функция dog, ускорен процесс получения картинок, добавлены оповещения процесса

--- laba1.py
+++ laba1.py
@@ -1,73 +1,69 @@
-from webbrowser import get
-from bs4 import BeautifulSoup
-import requests
-import re
-from re import sub
-from decimal import Decimal
-import io
-from datetime import datetime
-import pandas as pd
 import os
-import cv2
-import pyautogui
+import time
+import requests
+from bs4 import BeautifulSoup
 
 dog_path = "C://Users/User/nuck figgers/dataset/dog"
 dataset_path ="C://Users/User/nuck figgers/dataset"
 cat_path = "C://Users/User/nuck figgers/dataset/cat"
 
-def key_creater(key):
-    d = ['']
-    j=0
-    for i in range(10):
-      for g in range(10):
-          for k in range(10):
-            d.append(str(j)+str(i)+str(g)+str(k))
-    return d[key]
 
-        
-
-def get_data_dog(url):
-    headers ={
-        "user-agent":"Dima"
+def get_data_dog(count_imgs):
+    
+    _headers ={
+        "User-Agent": "Dima"
     }
     
-    for i in range(3,10):
-        params={'p':i}    
-        req=requests.get(url,params)
-        print(req.url)
+    count=0
+    
+    for i in range(0,10):
+        
+        url = f"https://yandex.ru/images/search?p={i}&text=dog&uinfo=sw-1920-sh-1080-ww-912-wh-881-pd-1.100000023841858-wp-16x9_2560x1440&lr=51&rpt=image"
+        req=requests.get(url, headers=_headers)
+  
+        soup = BeautifulSoup(req.text, "html.parser")
 
-        with open("dogs.html", "w", encoding="utf-8") as file:
-            file.write(req.text)
-            
-        with open("dogs.html", encoding="utf-8") as file:
-            src = file.read()
-            
-        soup = BeautifulSoup(src, "lxml")
-
+        src_list=[]
+        
         try:
-            images = soup.find_all("img", class_ ="justifier__thumb")
+            for link in soup.find_all("img", class_="justifier__thumb"):
+                src_list.append(link.get("src"))
+            print("Картинки успешно сканированы")
         
         except Exception:
-            images = "Нет картинок"
+            print("Нет картинок")
         
-        count = 1
-        for img in images:
-            
-            url = img['src']
-            source = "https:" + url
-            picture = requests.get(source)
-            
-            if not os.path.exists(dataset_path):
-                os.mkdir(dataset_path)   
-            if not os.path.exists(dog_path):
-                    os.mkdir(dog_path)  
-                        
-            out = open(dog_path + '/' + str(key_creater(count)) + '.jpg', 'wb') 
-            out.write(picture.content)
-            out.close()
-            
-            count+=1
         
+        for img_url in src_list:
+            if img_url.find("n=13") !=-1:
+                try:
+                    source = "https:" + img_url
+                    picture = requests.get(source)
+                    
+                    if not os.path.exists(dataset_path):
+                        os.mkdir(dataset_path)   
+                    if not os.path.exists(dog_path):
+                            os.mkdir(dog_path)  
+                    
+                    name_file=str(count)
+                          
+                    out = open(dog_path + '/' + name_file.zfill(4) + '.jpg', 'wb') 
+                    out.write(picture.content)
+                    out.close()
+                    
+                    count+=1
+                    if(count==count_imgs):
+                        return
+                    
+                except Exception:
+                    print("Error in: ", count)
+        
+
+        for sec in range(1,61):
+            print("Осталось ", 60-sec)
+            time.sleep(1)
+                    
+       
     
 def get_data_cat(url):
     headers ={
@@ -84,7 +80,7 @@ def get_data_cat(url):
     soup = BeautifulSoup(src, "lxml")
     
     try: 
-        images = soup.find_all("img", class_ ="justifier__thumb")
+        images = soup.fin_all("img", class_ ="justifier__thumb")
     
     except Exception:
         images = "Нет картинок"
@@ -107,42 +103,8 @@ def get_data_cat(url):
         out.close()
         
         count += 1
-        
-get_data_dog("https://yandex.ru/images/search?text=%20dog")
+
+ 
+count_find=100   
+get_data_dog(count_find)
 #get_data_cat("https://yandex.ru/images/search?text=cat")
-
-
-
-
-
-
-
-
-
-
-# _url = 'https://yandex.ru/images/search?text=%20dog'
-# _html_page = requests.get(_url, headers={'User-Agent':'Dima'}).text
-# _soup = BeautifulSoup(_html_page, 'lxml')
-# _ad = _soup.find_all('img', class_ = 'serp-item__thumb justifier__thumb')
-
-
-# count = 0
-# for i in range(len(_ad)):
-#     if _ad[i]['src']:
-#         count += 1 
-
-
-# #os.mkdir('C://Users/User/nuck figgers/dataset/cat')
-
-
-# _https = 'https:'
-# _dog_path = 'C://Users/User/nuck figgers/dataset/dog'
-# for i in range(count):
-#     _src = _https + _ad[i]['src']
-#     _picture = requests.get(_src)
-#     if not os.path.exists(_dog_path):
-#          os.mkdir(_dog_path)
-#     out = open(_dog_path + '/' + str(i) + '.jpg', 'wb') 
-#     out.write(_picture.content)
-#     out.close()
-
