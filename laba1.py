3223af47f17ac01666ccd4fb0d7270b8f0e24144
dmgorshenin <gorsh03@gmail.com>
2022-10-06 14:42:05 +0400

    Функция переделана для универсального скачивания картинок, а также доделана функция удаления копий и доскачивания новых картинок до минимального значения

--- laba1.py
+++ laba1.py
@@ -2,62 +2,52 @@ import os
 import string
 import random
 import time
+from webbrowser import get
 import requests
 from bs4 import BeautifulSoup
 import cv2
 import numpy as np
+from tqdm import tqdm
 
-dog_path = "C://Users/User/nuck figgers/dataset/dog"
-dataset_path ="C://Users/User/nuck figgers/dataset"
-cat_path = "C://Users/User/nuck figgers/dataset/cat"
+dog_path = "C:/Users/User/nuck figgers/dataset/dog"
+cat_path = "C:/Users/User/nuck figgers/dataset/cat"
 
-
-def get_data_dog(count_imgs):
+def get_images(count_imgs, path, typename):
     
-    if not os.path.exists(dataset_path):
-        os.mkdir(dataset_path)   
-    if not os.path.exists(dog_path):
-        os.mkdir(dog_path)
+    if not os.path.exists("C:/Users/User/nuck figgers/dataset"):
+        os.mkdir("C:/Users/User/nuck figgers/dataset")
+    if not os.path.exists(path):
+        os.mkdir(path) 
     
     count=0
     
-    for i in range(0,999):
+    for i in tqdm(range(3,999), desc="Страница "):
         
         letters = string.ascii_lowercase
         rand_string = ''.join(random.sample(letters, 10))
-        
         _headers ={
             "User-Agent": rand_string
         }
         
-        url = f"https://yandex.ru/images/search?p={i}&text=dog&uinfo=sw-1920-sh-1080-ww-912-wh-881-pd-1.100000023841858-wp-16x9_2560x1440&lr=51&rpt=image"
+        url = f"https://yandex.ru/images/search?p={i}&text={typename}&"
         req=requests.get(url, headers=_headers)
   
         soup = BeautifulSoup(req.text, "html.parser")
 
         src_list=[]
+
+        for link in soup.find_all("img", class_="justifier__thumb"):
+             src_list.append(link.get("src"))
         
-        try:
-            for link in soup.find_all("img", class_="justifier__thumb"):
-                src_list.append(link.get("src"))
-            print("Картинки успешно сканированы")
-        
-        except Exception:
-            print("Нет картинок")
-        
-        
-        for img_url in src_list:
+        for img_url in tqdm(src_list, desc="Скчивание картинок "):
             if img_url.find("n=13") !=-1:
                 try:
-                    print("Картинка", count)
                     source = "https:" + img_url
                     picture = requests.get(source)
                     
-                      
-                    
                     name_file=str(count)
-                          
-                    out = open(dog_path + '/' + name_file.zfill(4) + '.jpg', 'wb') 
+                    
+                    out = open(path + '/' + name_file.zfill(4) + '.jpg', 'wb') 
                     out.write(picture.content)
                     out.close()
                     
@@ -70,75 +60,18 @@ def get_data_dog(count_imgs):
                 except Exception:
                     print("Error in: ", count)
                     
-       
-    
-def get_data_cat(count_imgs):
-    
-    if not os.path.exists(dataset_path):
-        os.mkdir(dataset_path)   
-    if not os.path.exists(cat_path):
-        os.mkdir(cat_path)
-        
-    count=0
-    
-    for i in range(0,999):
-        
-        letters = string.ascii_lowercase
-        rand_string = ''.join(random.sample(letters, 10))
-        _headers ={
-            "user-agent":rand_string
-        } 
-        
-        url = f"https://yandex.ru/images/search?p={i}&text=cat&uinfo=sw-1920-sh-1080-ww-878-wh-924-pd-1-wp-16x9_1920x1080&lr=51&rpt=image"
-        req=requests.get(url, headers=_headers)
-  
-        soup = BeautifulSoup(req.text, "html.parser")
+                         
 
-        src_list=[]
-        
-        try:
-            
-            for link in soup.find_all("img", class_="justifier__thumb"):
-                src_list.append(link.get("src"))
-            print("Картинки успешно сканированы")
-        
-        except Exception:
-            print("Нет картинок")
-        
-        
-        for img_url in src_list:
-            if img_url.find("n=13") !=-1:
-                try:
-                    print("Картинка", count)
-                    source = "https:" + img_url
-                    picture = requests.get(source)
-                    
-                    name_file=str(count)
-                          
-                    out = open(cat_path + '/' + name_file.zfill(4) + '.jpg', 'wb') 
-                    out.write(picture.content)
-                    out.close()
-                    
-                    time.sleep(0.25)
-                    
-                    count+=1
-                    if(count==count_imgs):
-                        return
-                    
-                except Exception:
-                    print("Error in: ", count)
-            
 def is_similar(image1, image2):
     return image1.shape == image2.shape and not(np.bitwise_xor(image1,image2).any())
     
 def check_images(path,count):
-    import tqdm
     c=count
     images = []
     for filename1 in os.listdir(path):
         images.append((cv2.imread(os.path.join(path, filename1)), os.path.join(path, filename1)))
     
-    for im, fname in tqdm.tqdm(images):
+    for im, fname in tqdm(images):
         for im2, fname2 in images:
             if(fname==fname2):
                 continue
@@ -148,37 +81,26 @@ def check_images(path,count):
                 c-=1;
     return count-c
                 
-    
-    #for filename1 in tqdm.tqdm(os.listdir(path)):
-    #    img1=cv2.imread(os.path.join(path, filename1))
-    #    for filename2 in os.listdir(path):
-    #        img2=cv2.imread(os.path.join(path, filename2))
-    #        
-    #        if(filename1==filename2):
-    #            continue
-    #        
-    #        if is_similar(img1,img2):
-    #            print(filename1, filename2)
-    #            #os.remove(filename2)
-            
-    
-
-
-
  
  
 count_find=1100 
 
-get_data_dog(count_find)
+get_images(count_find, dog_path, "dog")
+
+print("Пауза")
+for sec in range(1,61):
+    print("Осталось ", 61-sec)
+    time.sleep(1)
+    
+get_images(count_find, cat_path, "cat")
+
+new_count=check_images(dog_path,count_find)
+get_images(new_count, dog_path, "dog")
 
 print("Пауза")
 for sec in range(1,61):
     print("Осталось ", 61-sec)
     time.sleep(1)
 
-get_data_cat(count_find)
-
-new_count=check_images(dog_path,count_find)
-get_data_cat(new_count)
 new_count=check_images(cat_path,count_find)
-get_data_dog(new_count)
+get_images(new_count, cat_path, "cat")
